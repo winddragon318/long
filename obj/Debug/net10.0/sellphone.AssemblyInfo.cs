dc92e21c2e9028d477b3cce7599e4925fff46e26
long <nguyenlong331188@gmail.com>
2025-12-09 16:21:51 +0700

    productdetail

--- obj/Debug/net10.0/sellphone.AssemblyInfo.cs
+++ obj/Debug/net10.0/sellphone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sellphone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa29ffd0f40125ae415a1e931b08cc90b2d52c33")]
 [assembly: System.Reflection.AssemblyProductAttribute("sellphone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sellphone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
